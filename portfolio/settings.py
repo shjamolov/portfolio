4cbd53cc9ea2a601a61ea40f05ec93313637b734
Shukhrat <shukhratjamolov304@gmail.com>
2025-05-15 11:09:39 +0500

    added command static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,6 +137,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 
 MEDIA_URL = '/media/'
